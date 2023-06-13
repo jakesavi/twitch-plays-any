3f705b317b521676f5905158c67788f4b156b8c2
jakesavi <jakesavi@buffalo.edu>
2023-06-13 14:33:10 -0400

    Added git ignore and config for API keys.

---
+++ main.py
@@ -0,0 +1,4 @@
+def getToken(USER, PASS):
+    #TODO This function should fetch a token from a user and have them input their credentials for twitch. If this works. we can then get their token to latch
+    #To their chat.
+    
