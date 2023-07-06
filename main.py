c5d457dc1b6be172ab540f79038f124a20fdfc60
jakesavi <jakesavi@buffalo.edu>
2023-07-05 21:12:05 -0400

    Added a lot of functionallity and a main function

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import json
 import socket
+import Secret
 import threading
-import PySimpleGUI as sg
+from pynput import keyboard
 ##EXAMPLE CODE TAKEN FROM THIS SCRIPT. THIS SCRIPT WAS USE AS A LAUNCHING POINT TO EXPAND THE SOCKET AVAILABILITY.
 ##EXAMPLE CODE: 
-
+##Put the file name of the json blob here!
+JSON: str = "test.json"
+##END OF JSON FILE :)
 SERVER: str = "irc.twitch.tv"
 PORT: int = 6667
 BOT: str = "TwitchPlaysAny"
 CHANNEL: str = "byte1223"
-
+OWNER: str = "byte1223"
 ###OATH CODE GOES HERE###
-PASS: str = ""
+PASS: str = Secret.PASS()
 ###END OF OATH###
-user: str = ""
-message: str = ""
 #Twitch is the connection object to chat. 
 #Utilize this in main to connect to chat.
 class Twitch:
@@ -22,22 +23,33 @@ class Twitch:
     def __init__(self) -> None:
         irc:socket = socket.socket()
         irc.connect((SERVER,PORT))
+        #irc.send("CAP REQ :twitch.tv/commands\n".encode())
         irc.send(("PASS " + PASS + "\n" +
-          "NICK " + BOT + "\n" +
-          "JOIN #" + CHANNEL + "\n").encode())
+            "NICK " + BOT + "\n" +
+            "JOIN #" + CHANNEL + "\n").encode())
+        #Loading the controls.
+        f = open(JSON)
+        data : dict= json.load(f)
+        self.control : dict = data.get("Controls")
+        print(self.control.get(list(self.control.keys())[0]))
+        self.irc: socket = irc
 
-        self.irc = irc
+
+
+    def sendMessage(self, message: str) -> None: 
+        messageTemp :str = "PRIVMSG #" + CHANNEL + " :" + message +"\n"
+        #Encode as bytes and ship off the message
+        self.irc.send(messageTemp.encode())
+
 
     def loadCompleate(self,line:str) -> bool:
         if ("End of /NAMES list" in line):
             print("TwitchBot has jointed " + CHANNEL + "'s Channel!")
-            self.sendMessage(self.irc, "Hello World")
+            self.sendMessage("Hello World")
             return False
         else:
             return True
         
-    def sendMessage(self ,irc: socket, message: str) -> None: 
-        messageTemp :str = "PRIVMSG #" + CHANNEL + " :" + message
 
     def connectToChat(self) -> None:
         Loading :bool = True
@@ -46,8 +58,50 @@ class Twitch:
             readbuffer_join:str = readbuffer_join.decode()
             for line in readbuffer_join.split("\n")[0:-1]:
                 print(line)
-                Loading = self.loadCompleate(line)
+                Loading :bool = self.loadCompleate(line)
+        #When done... Latch to chat and read chat messages!
+
+
+    def reciever(self) -> None:
+        while True:
+            bufferRecieved:bytes = self.irc.recv(1024)
+            bufferRecieved:str = bufferRecieved.decode()
+            for line in bufferRecieved.split("\n")[0:-1]:
+                print(line)
+                #Prevent premature connection termination
+                if "PING :tmi.twitch.tv" in line:
+                    self.irc.send("PONG :tmi.twitch.tv".encode())
+                    print("PONG :tmi.twitch.tv") 
+                if ("PRIVMSG" and "$" in line):
+                    #Send Line to Handler Ensure that another thread runs this...
+                    command: str = line.split('$')[1]
+                    command: str = command.strip("\n")
+                    
+                    newThread = threading.Thread(target=self.Handler, args=[command]) 
+                    newThread.run()
+
+#TODO Fix the fucking handler. This is killing me... :(
+    def Handler(self, msg:str):
+        #Handler Needs to check the Json blob of data and ensure that it exists in the dictionary
+        #Had a weird bug here. Couldn't use in on this json blob?????
+        found=False
+        for key, val in self.control.items():
+            if key == msg:
+                found=True
+                print("I FOUND THE VALUE")
+            else:
+                continue
+
+def main():
+    t: Twitch = Twitch()
+    #Start by connecting to chat.
+    t.connectToChat()
+    #Then run the reciever.
+    t.reciever()
     
 
-def reciever() -> None:
-    #TODO? 
+
+    
+
+
+main()
