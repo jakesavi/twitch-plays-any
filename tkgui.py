ab7bec7110bad895650ce4737fc2b3c63851bef4
jakesavi <jakesavi@buffalo.edu>
2023-09-08 11:46:23 -0400

    Added weird label functionallity that i do not like... TODO figure out how to make this window look more appealing.

--- tkgui.py
+++ tkgui.py
@@ -1,36 +1,74 @@
 from tkinter import *
-from tkinter import ttk 
-
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText  
+import threading
+import json
 #TODO Figure out how to have multiple displays.
-titleDisplay:str = "N/A"
-DescriptionDisplay:str = "No Json made or selected. Please Import one or click \"New JSON\""
-
-def FormNewJson():
-    newJsonInfo = Tk(className="New Json")
-    popUp= ttk.Frame(newJsonInfo, padding=10)
-    popUp.grid()
-    ttk.Label(popUp, Text="Title of Game").grid(row=0)
-    ttk.Label(popUp, Text ="Description").grid(row=1)
-    title : Entry = ttk.Entry(popUp).grid(row=0,column=1)
-    description: Entry = ttk.Entry(popUp).grid(row=1,column=1)
-    ttk.Button(popUp, text="Done",command=lambda: CloseAndSaveJSON(title.get(), description.get(),popUp))
-    popUp.mainloop()
-
-def CloseAndSaveJSON(title:str, description:str, window:Frame):
-    global titleDisplay
-    global DescriptionDisplay
-    titleDisplay = title
-    DescriptionDisplay = description
-    window.destroy()
-
-about: str = """Welcome to Twitch plays any!\nHow to use: Form a JSON utilizing the key-capture tool below. You can also Import and Export your own."""
-main = Tk(className="Main")
-frm= ttk.Frame(main, padding=50)
-frm.grid(padx=10,pady=10)
-ttk.Label(frm, text=about).grid(column=0,row=0)
-ttk.Widget(frm,"text").grid(column=0,row=1)
-ttk.Button(frm, text="New JSON", command=FormNewJson ).grid(column=1, row=1)
-ttk.Button(frm, text="Quit", command=main.destroy).grid(column=2,row=2)
-main.mainloop()
+#Solution. Create Multiple threads using python threads.
+#ISSUES: For some god damn reason my python interpriter really hates widget types form tkinter. Why? No idea.
+# Right now the compiler is saying they are "None_Type" why? who tf knows...
+# SOLVED: Python takes last function evaluated type in this case. grid() as seen in function "FormNewJsonWindow(Self)"
+titleDisplay:str = None
+DescriptionDisplay:str = None
+class App:
+    def __init__(self) -> None:
+        self.t1 = threading.Thread(target=self.FormNewJsonWindow)
+        self.main = Tk(className="Main") #Run Main HERE So we can access the main thread elsewhere throughout the object
+        self.MadeJson: dict[str, any] = {}
+        self.WindowLaunch()
+        
 
 
+    def FormNewJsonWindow(self) -> None:
+        newJsonInfo = Tk(className="New Json")
+        popUp= ttk.Frame(newJsonInfo, padding=25)
+        popUp.grid()
+        ttk.Label(popUp, text="Title").grid(row=0)
+        ttk.Label(popUp, text="Description").grid(row=1)
+        title : ttk.Entry = ttk.Entry(popUp)
+        title.grid(row=0,column=1)
+        description: ttk.Entry = ttk.Entry(popUp)
+        description.grid(row=1,column=1)
+        DoneButton: ttk.Button = ttk.Button(popUp, text="Done",command=lambda: self.CloseAndSaveJSON(title.get(), description.get(),window=newJsonInfo))
+        DoneButton.grid(row=2,column=1)
+        popUp.mainloop() 
+
+
+    def FormNewJson(self):
+        self.t1.run()
+
+    def CloseAndSaveJSON(self, title:str, description:str, window:Tk):
+        self.MadeJson={"Title": title, "Description": description, "Controls": {}}
+        window.destroy()
+        print(self.MadeJson)
+        #Write Made Json to a file.
+        with open(f"{self.MadeJson['Title']}.json", "w") as f:
+            f.write(json.dumps(self.MadeJson))
+            f.close()
+        
+
+
+
+    def WindowLaunch(self) -> None: 
+        about: str = """Welcome to Twitch plays any!\n
+        How to use: Form a JSON utilizing the key-capture tool below. You can also Import and Export your own. Once you make a new JSON it will appear in the text field below.\n
+        NOTE: The text field below is not editable. If you wish to edit the JSON directly simply open it in notepad."""
+        frm= ttk.Frame(self.main, padding=50)
+        frm.grid(padx=10,pady=10)
+        ttk.Label(frm, text=about).grid(column=0,row=0)
+        #TODO Change this widget to be READONLY and 
+        ttk.Label(frm, text=json.dumps(self.MadeJson), background="white", width=25).grid(column=0,row=1)
+        ttk.Button(frm, text="New JSON", command=self.FormNewJson ).grid(column=1, row=1)
+        ttk.Button(frm, text="Quit", command=self.main.destroy).grid(column=2,row=2)
+        self.main.mainloop()
+
+
+
+
+
+
+def Main():
+    instance:App = App()
+
+
+Main()
