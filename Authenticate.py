431085b9aa4235176f25b8006aac54e1eca7c6fb
Jake S <jws2460@gmail.com>
2024-03-03 10:23:43 -0500

    Added Reconnect Function, and connect function.

--- Authenticate.py
+++
@@ -1,6 +0,0 @@
-import socket
-class Authenticate:
-    
-    def AuthenticationListener(self)-> None:
-        #Assuming port value was not changed utilized port 3000, Set up the same way as before. 
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- TwitchConnect.py
+++ TwitchConnect.py
@@ -1,28 +1,27 @@
 import socket
 import time
+import random
 import threading
 import webbrowser
+from Authenticate import Authenticate
 class Twitch:
     sock: socket = None
-    # You may hardcode the OAuthToken however, it is recommended you don't... As the application will redirect you to the auth, and fetch the Token itself.
-    OAuthToken: str = None 
+    channel: str = "byte1223"
+    authenticator: Authenticate = Authenticate #Creating class instance in self for data storage.
     # Connect to the IRC server utilizing socket connection. 
     clientToken: str = 'bm5jz15swcewiz2igffjr24h7ijkdl'
     def twitch_connect(self, channel: str) -> None:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
         print("Connecting to Twitch...")
         # Specified twitch socket info.
         self.sock.connect(('irc.chat.twitch.tv', 6667))
-        # Verify User Utilizing OAUTH or connect anonymously
-        if self.OAuthToken == None:
-            print("Awaiting Server Authentication...")
-            # Spin up a listener for Token here! TODO
-            
-            # Open up the web browser. 
-            webbrowser.open(f'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={self.clientToken}&redirect_uri=http://localhost:3000/&scope=chat%3Aread+user%3Awrite%3Achat+user%3Abot&state=c3ab8aa609ea11e793ae92361f002671')
-
-
+        # Connect anonymously.
+        user : str = 'justinfan%i' % random.range(100,99999)
+        self.sock.send(('NICK %s\r\nPASS fakefan\r\n' % user).encode())
+    def reconnect(self, delay: float) -> None:
+        time.sleep(delay)
+        self.reconnect()
+        
 ## TEST CODE BELOW.
 twi = Twitch()
 twi.twitch_connect('byte1223')
